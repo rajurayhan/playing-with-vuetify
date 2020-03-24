9811cdc0600850beb02908d54d055cf89f193a4a
Raju Rayhan <raju.rayhan@yandex.com>
2020-03-24 09:25:43 +0600

    Added todays tasks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,9 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <example-component>
+            
+                    </example-component>
                 </div>
             </div>
         </div>
